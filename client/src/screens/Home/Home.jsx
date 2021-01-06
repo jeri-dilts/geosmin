958d3f4ed5a29c0adf09858a019b2c5b65935f63
snakpak <mike.pakulak@gmail.com>
2021-01-06 09:50:43 -0500

    Updated home page, linked to all products

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import './Home.css';
-import Layout from '../../components/shared/Layout/Layout';
-import ProductCards from '../../components/ProductCards/ProductCards'
+import React from "react";
+import "./Home.css";
+import Layout from "../../components/shared/Layout/Layout";
+import ProductCards from "../../components/ProductCards/ProductCards";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <Layout>
-      <div className='homeIMG'>
-        <img className='home-image' src='https://images.unsplash.com/photo-1582095128060-e9ca8130cc6b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80' />
+      <div className="homeIMG">
+        <img
+          className="home-image"
+          src="https://images.unsplash.com/photo-1582095128060-e9ca8130cc6b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
+        />
       </div>
-      <div className='product-row'>
-        <div className='top'>
+      <div className="product-row">
+        <div className="top">
           <h2>Top Sellers</h2>
-          <img className='topIMG' src='https://images.unsplash.com/photo-1558616629-899031969d5e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80' />
-          </div>
-        <div className='featured'>New & Featured</div>
-        <div className='all'>All Products</div>
+          <img
+            className="topIMG"
+            src="https://images.unsplash.com/photo-1558616629-899031969d5e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          />
+        </div>
+        <div className="featured">
+          <h2>New & Featured</h2>
+          <img
+            className="featuredIMG"
+            src="https://images.unsplash.com/photo-1593850685222-689a0efd7957?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+          />
+        </div>
+        <div className="all" to="/products">
+          <h2>All Products</h2>
+          <NavLink to="/products">
+            <img
+              className="allIMG"
+              src="https://images.unsplash.com/photo-1501169527804-c216a681aab8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+            />
+          </NavLink>
+        </div>
       </div>
-    <div className="products">
-      <ProductCards />
-    </div>
     </Layout>
   );
 };
