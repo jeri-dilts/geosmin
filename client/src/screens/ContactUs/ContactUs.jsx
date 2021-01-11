0375e6303c1d315aeeccb63ffa73253684fc4316
Hunter <huntermott42@gmail.com>
2021-01-10 18:42:31 -0700

    added footer to contact us

--- client/src/screens/ContactUs/ContactUs.jsx
+++ client/src/screens/ContactUs/ContactUs.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContactUsForm from '../../components/ContactUsForm/ContactUsForm';
+import Footer from '../../components/shared/Footer/Footer';
 import Layout from '../../components/shared/Layout/Layout';
 
 const ContactUs = () => {
   return (
     <div>
       <Layout>
-      <ContactUsForm />
+        <ContactUsForm />
+        <Footer />
       </Layout>
     </div>
   );
